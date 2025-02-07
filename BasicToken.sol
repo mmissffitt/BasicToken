806c640ca331eb6eba33219f2f86c61ca32afafd
mmissffitt <tade175@bk.ru>
2025-02-07 11:22:16 +0300

    Update BasicToken.sol

--- BasicToken.sol
+++ BasicToken.sol
@@ -9,6 +9,8 @@ contract BasicToken is IERC20 {
     uint8 public decimals = 18;
     uint256 public totalSupply;
 
+    uint256 public transferFee = 2;
+
     mapping(address => uint256) public balanceOf;
     mapping(address => mapping(address => uint256)) public allowance;
 
@@ -18,23 +20,30 @@ contract BasicToken is IERC20 {
     }
 
     function transfer(address _to, uint256 _value) public virtual override returns (bool success) {
-        require(balanceOf[msg.sender] >= _value, "Insufficient balance");
-        balanceOf[msg.sender] -= _value;
+        revertOnZeroValueTransfers(_value);
+        revertOnTransferToZeroAddress(_to);
+        uint256 fee = calculateFee(_value);
+        require(balanceOf[msg.sender] >= _value + fee, "Insufficient balance");
+        balanceOf[msg.sender] -= _value + fee;
         balanceOf[_to] += _value;
         emit Transfer(msg.sender, _to, _value);
         return true;
     }
 
     function approve(address _spender, uint256 _value) public virtual override returns (bool success) {
+        revertOnApprovalToZeroAddress(_spender);
         allowance[msg.sender][_spender] = _value;
         emit Approve(msg.sender, _spender, _value);
         return true;
     }
 
     function transferFrom(address _from, address _to, uint256 _value) public virtual override returns (bool success) {
-        require(_value <= balanceOf[_from], "Insufficient balance");
+        revertOnZeroValueTransfers(_value);
+        revertOnTransferToZeroAddress(_to);
+        uint256 fee = calculateFee(_value);
+        require(_value + fee <= balanceOf[_from], "Insufficient balance");
         require(_value <= allowance[_from][msg.sender], "Insufficient allowance");
-        balanceOf[_from] -= _value;
+        balanceOf[_from] -= _value + fee;
         balanceOf[_to] += _value;
         allowance[_from][msg.sender] -= _value;
         emit Transfer(_from, _to, _value);
@@ -54,5 +63,21 @@ contract BasicToken is IERC20 {
         totalSupply -= _value;
         emit Transfer(msg.sender, address(0), _value);
         return true;
+    }
+
+    function revertOnApprovalToZeroAddress(address _spender) internal pure {
+        require(_spender != address(0), "Cannot approve the zero address");
+    }
+
+    function revertOnZeroValueTransfers(uint256 _value) internal pure {
+        require(_value > 0, "Cannot transfer zero value");
+    }
+
+    function revertOnTransferToZeroAddress(address _to) internal pure {
+        require(_to != address(0), "Cannot transfer to the zero address");
+    }
+
+    function calculateFee(uint256 _value) internal view returns (uint256) {
+        return (_value * transferFee) / 100;
     }
 }
